53a8dde41e7571d3cce6e8228afbce5495444369
hcq <785353176@qq.com>
2020-12-10 15:35:36 +0800

    第二次进行数据提交

--- TestModule01/src/Student.java
+++
@@ -1,6 +0,0 @@
-/**
- * @author: DoubleQ
- * @Date: 2020-12-10
- **/
-public class Student {
-}
--- TestModule01/src/com/hcq/Student.java
+++ TestModule01/src/com/hcq/Student.java
@@ -1,8 +1,8 @@
-package com;
+package com.hcq;
 
 /**
  * @author: DoubleQ
  * @Date: 2020-12-10
  **/
-public class hcq {
+public class Student {
 }
